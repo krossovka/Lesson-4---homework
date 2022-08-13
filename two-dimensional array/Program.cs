f4abbf5c12afa78202cb19fff2e3bbe58fc62d79
krossovka <krossovka@outlook.com>
2022-08-13 18:21:18 +0300

    some changes

---
+++ two-dimensional array/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите количество элементов в массиве");
+int sizeOfArray1 = Convert.ToInt32(Console.ReadLine());
+int sizeOfArray2 = Convert.ToInt32(Console.ReadLine());
+
+int[ , ] array = new int[sizeOfArray1, sizeOfArray2];
+for (int i = 0; i < array.Length; i++)
+{
+    Random random = new Random();
+    int value = random.Next(-100, 100);
+    array[i, i] = value;
+    Console.Write(array[i, i] + " ");
+}
